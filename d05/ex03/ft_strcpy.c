a44f7145d85c11c00a91382fac3b9bc0de5018f9
teemu-hakala <78655964+teemu-hakala@users.noreply.github.com>
2021-10-10 11:56:04 +0300

    d05ex03 return value

--- d05/ex03/ft_strcpy.c
+++ d05/ex03/ft_strcpy.c
@@ -6,14 +6,17 @@
 /*   By: teemuhakala <teemuhakala@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 03:51:08 by thakala           #+#    #+#             */
-/*   Updated: 2021/10/03 20:27:29 by teemuhakala      ###   ########.fr       */
+/*   Updated: 2021/10/10 11:40:56 by teemuhakala      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcpy(char *dest, char *src)
 {
+	char	*result;
+
+	result = dest;
 	while (*src)
 		*dest++ = *src++;
 	*dest = *src;
-	return (dest);
+	return (result);
 }
